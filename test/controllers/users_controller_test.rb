f35839be3fd554a4e520ace07e92728a522f4902
Timothy Alexis Vass <timvas@bredband2.se>
2022-09-29 08:42:46 +0200

    Add friendly forwarding

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:test)
-    @other_user = users(:test2)
+    @testuser = users(:test)
+    @testuser_2 = users(:test2)
   end
 
   test "should get signup page" do
@@ -12,16 +12,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect away from edit page when logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
+    log_in_as(@testuser_2)
+    get edit_user_path(@testuser)
     assert flash.empty?
     assert_redirected_to root_url
   end
 
   test "should redirect away from update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: {
-      user: { name: @user.name, email: @user.email }
+    log_in_as(@testuser_2)
+    patch user_path(@testuser), params: {
+      user: { name: @testuser.name, email: @testuser.email }
     }
     assert flash.empty?
     assert_redirected_to root_url
