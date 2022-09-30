f0e91585e75a4b2f2eba3b082a25648e969acbcc
Timothy Alexis Vass <timvas@bredband2.se>
2022-09-30 17:01:40 +0200

    Complete password reset controller and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,9 +58,15 @@ class User < ApplicationRecord
     update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
-    # Sends password reset email.
+  
+  # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
