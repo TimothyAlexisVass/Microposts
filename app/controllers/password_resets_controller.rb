918091f1fbd541b64ecd3477e1911ed2fca3a9e9
Timothy Alexis Vass <timvas@bredband2.se>
2022-09-30 17:15:35 +0200

    Complete password reset system

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,6 +29,7 @@ class PasswordResetsController < ApplicationController
     elsif
       @user.update_attributes(user_params)
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
