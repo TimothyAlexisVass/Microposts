606759e659092c593de94f158ba17b911d02bab4
Timothy Alexis Vass <timvas@bredband2.se>
2022-10-04 09:18:57 +0200

    Add relationships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   before_create :create_activation_digest
 
   has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2, maximum: 255 }
   validates :email, presence: true,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221004061726) do
+ActiveRecord::Schema.define(version: 20221004065854) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -27,9 +27,6 @@ ActiveRecord::Schema.define(version: 20221004061726) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
