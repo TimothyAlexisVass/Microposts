50540de0c1e739e5be28afe1f3bae742882059f6
Timothy Alexis Vass <timvas@bredband2.se>
2022-10-05 11:02:45 +0200

    Complete follower functionality with AJAX

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
   
   def setup
     @user = users(:test1)
+    @user2 = users(:test4)
     log_in_as(@user)
   end
   test "following page" do
@@ -21,6 +22,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |follower|
       assert_select "a[href=?]", user_path(follower)
+    end
+  end
+
+  test "should follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @user2.id }
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, xhr: true, params: { followed_id: @user2.id }
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@user2)
+    relationship = @user.following_a_user.find_by(followed_id: @user2.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@user2)
+    relationship = @user.following_a_user.find_by(followed_id: @user2.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), xhr: true
     end
   end
 end
